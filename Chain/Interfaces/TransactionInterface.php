3cb814a0bd8d3f8f384c66bba70abf63d66ccd69
Its Me <collinge_9@hotmail.co.uk>
2025-07-15 19:50:38 +0100

    Added all *required interfaces
    * I think all these are required - for now

---
+++ Chain/Interfaces/TransactionInterface.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Copyright (c) 2025. ChainBase Project
+ *  This file is part of ChainBase, a PHP-based EVM-compatible Layer 2 blockchain framework.
+ *  Licensed under the MIT License. See the LICENSE file for details.
+ */
+
+namespace Chain\Interfaces;
+
+interface TransactionInterface
+{
+
+}
